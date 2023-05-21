28053526c0ab6deefbfead81bacc8cac58cef519
chienzen <134183802+chienzen@users.noreply.github.com>
2023-05-21 10:42:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+const  CONFIG  = {ILOVEUOU 
+                  
+    introTitle : NHUNG
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
